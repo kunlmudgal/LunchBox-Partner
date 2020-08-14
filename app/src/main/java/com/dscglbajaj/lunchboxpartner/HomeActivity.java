85a1ca01cbe6f8cd42b8cb32b798a08b2d08549b
MAYANK TYAGI <34006835+tmayank860@users.noreply.github.com>
2020-08-14 15:51:07 +0530

    Update the database

--- app/src/main/java/com/dscglbajaj/lunchboxpartner/HomeActivity.java
+++ app/src/main/java/com/dscglbajaj/lunchboxpartner/HomeActivity.java
@@ -1,22 +1,26 @@
 package com.dscglbajaj.lunchboxpartner;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
     Button btn_logout;
+    private Toolbar myToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
         btn_logout=findViewById(R.id.logout_button);
         btn_logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +30,10 @@ public class HomeActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+    }
+
+    private void setSupportActionBar(Toolbar myToolbar) {
 
     }
 }
